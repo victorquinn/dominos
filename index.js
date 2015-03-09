4fd749be54bde1749a5d43b92d8837a7fe50806c
Victor Quinn <victor@socialradar.com>
2015-03-09 18:28:55 -0400

    Add store info and menue

--- index.js
+++ index.js
@@ -14,13 +14,42 @@ app.get('/v1/stores', function(req, res) {
         });
     } else {
         console.log('returning stores for ' + address);
-        dominos.store.find(
-            address,
-            function(storeData){
-                res.status(200).json(storeData.result.Stores);
-            }
-        );
+        dominos.store.find(address, function(storeData) {
+            res.status(200).json(storeData.result.Stores);
+        });
     }
+});
+
+app.get('/v1/stores/:store_id', function(req, res) {
+    var store_id = req.params.store_id;
+    console.log('returning info for store ' + store_id);
+    dominos.store.info(store_id, function(storeData) {
+        if (storeData.success === false) {
+            res.status(400).json({
+                code: 'INVALID_STORE',
+                title: 'Invalid Store',
+                detail: 'Cannot retrieve the menu for a store with id `' + store_id + '`'
+            });
+        } else {
+            res.status(200).json(storeData);
+        }
+    });
+});
+
+app.get('/v1/stores/:store_id/menu', function(req, res) {
+    var store_id = req.params.store_id;
+    console.log('returning info for store ' + store_id);
+    dominos.store.menu(store_id, function(storeData) {
+        if (storeData.success === false) {
+            res.status(400).json({
+                code: 'INVALID_STORE',
+                title: 'Invalid Store',
+                detail: 'Cannot retrieve the menu for a store with id `' + store_id + '`'
+            });
+        } else {
+            res.status(200).json(storeData);
+        }
+    });
 });
 
 var port = process.env.PORT || 5000;

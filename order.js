370ee6283ecb56c0d4fe37d286379e3b6cbf7a6d
Victor Quinn <victor@socialradar.com>
2015-03-10 14:36:38 -0400

    Now actually placing order

--- order.js
+++ order.js
@@ -1,5 +1,6 @@
 var dominos = require('dominos');
 var debug = require('debug')('pizza:cheese');
+var creditcard = require('creditcard');
 
 module.exports = {
     cheese: function(req, res) {
@@ -15,6 +16,8 @@ module.exports = {
         //         "postalcode": "20036"
         //     }
         // }
+
+        // @todo add validation
 
         // First, make the order
         debug("The body is %s", JSON.stringify(req.body, null, 4));
@@ -40,16 +43,55 @@ module.exports = {
         cheesePizza.Code='14SCREEN';
         order.Order.Products.push(cheesePizza);
 
+        // Add payment info
+        var card = new dominos.class.Payment();
+        if (!creditcard.validate(req.body.creditcard.number)) {
+            res.status(400).json({
+                code: 'INVALID_CREDIT_CARD',
+                title: 'Invalid credit card',
+                detail: 'Cannot order pizza with an invalid credit card'
+            });
+        }
+
+        card.Number = req.body.creditcard.number;
+        card.CardType = creditcard.cardscheme(req.body.creditcard.number).toUpperCase();
+        card.SecurityCode = req.body.creditcard.security;
+        card.PostalCode = req.body.creditcard.postalcode;
+        card.Expiration = req.body.creditcard.expiration;
+
         debug('order before being sent: %s', JSON.stringify(order, null, 2));
 
         // Validate this order
         dominos.order.validate(order, function(data) {
-            debug('validated order: %s', JSON.stringify(data, null, 2));
+            var order_id = data.result.Order.OrderID;
             if (data.success) {
-                res.status(200).json({
-                    order_id: data.result.Order.OrderID
+                dominos.order.price(order, function(data) {
+                    var amount = {
+                        total: data.result.Order.Amounts.Payment,
+                        subtotal: data.result.Order.Amounts.Net,
+                        tax: data.result.Order.Amounts.Tax
+                    };
+
+                    card.Amount = amount.total;
+                    order.Order.Payments.push(card);
+                    //                    debug('')
+                    dominos.order.place(order, function(data) {
+                        if (data.result.Status === -1) {
+                            res.status(400).json({
+                                code: data.result.Order.CorrectiveAction.Code,
+                                detail: data.result.Order.CorrectiveAction.Detail
+                            });
+                        } else {
+                            debug('ORDER SUCCESSFUL!');
+                            res.status(200).json({
+                                order_id: order_id,
+                                amount: amount
+                            });
+                        }
+                    });
                 });
             } else {
+                // Need to better handle failure if no address or something
                 res.status(400).json(data.result);
             }
         });
